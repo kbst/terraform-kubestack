6722666952b9ab10c9b506302fcb5ab151902d9d
Philipp Strube <philipp.strube@container-solutions.com>
2019-01-03 14:42:41 +0100

    Fix remove empty element from concatenated scope list

--- google/_modules/gke/cluster.tf
+++ google/_modules/gke/cluster.tf
@@ -8,7 +8,7 @@ locals {
     "https://www.googleapis.com/auth/trace.append",
   ]
 
-  oauth_scopes = "${concat(local.base_oauth_scopes, var.extra_oauth_scopes)}"
+  oauth_scopes = "${compact(concat(local.base_oauth_scopes, var.extra_oauth_scopes))}"
 }
 
 resource "google_container_cluster" "current" {

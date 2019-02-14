70eb0b1757812f1ef75da65d3a0e92955ce48c1e
Philipp Strube <pst@kubestack.com>
2019-02-14 19:29:43 +0100

    Handle service account activation and improve getting the user name

--- google/_modules/gke/gcloud_config_account.sh
+++ google/_modules/gke/gcloud_config_account.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-USER="$(gcloud config get-value account)"
+if [ ! -z "$GOOGLE_APPLICATION_CREDENTIALS" ]
+then
+    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+fi
+
+USER="$(gcloud auth list --filter=status:ACTIVE --format='value(account)')"
 
 jq -n --arg user "$USER" '{"user":$user}'

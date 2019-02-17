f9886269164e97727ed63028087c72e9b14b246a
Philipp Strube <pst@kubestack.com>
2019-02-17 02:28:49 +0100

    Add provider version constraints fix #23

---
+++ google/cluster/providers.tf
@@ -0,0 +1,19 @@
+provider "external" {
+  version = "~> 1.0"
+}
+
+provider "google-beta" {
+  version = "~> 2.0"
+}
+
+provider "kubernetes" {
+  version = "~> 1.5"
+}
+
+provider "null" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}

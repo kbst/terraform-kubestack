0d32074d1583448f270f6d24d48ef02556c4d050
Spazzy <brendankamp757@gmail.com>
2019-03-14 16:07:43 +0100

    Switched to use the git repo as source

--- quickstart/src/configurations/do_ks/clusters.tf
+++ quickstart/src/configurations/do_ks/clusters.tf
@@ -1,4 +1,4 @@
 module "do_zero" {
-  source = "../../../../digitalocean/cluster"
+  source = "github.com/kbst/terraform-kubestack//digitalocean/cluster?ref={{version}}"
   configuration = "${var.clusters["do_ks_zero"]}"
 }

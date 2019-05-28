32e72eeeab2d7dbe0f28334c299a07203e137f88
Gregory Damiani <gregory.damiani@yara.com>
2019-05-28 13:38:46 +0200

    add --role to authenticator shell script

--- aws/_modules/eks/provider_authenticator.sh
+++ aws/_modules/eks/provider_authenticator.sh
@@ -5,7 +5,7 @@ set -e
 eval "$(jq -r '@sh "CLUSTER_NAME=\(.cluster_name)"')"
 
 # Retrieve token with Heptio Authenticator
-TOKEN=$(aws-iam-authenticator token -i $CLUSTER_NAME | jq -r .status.token)
+TOKEN=$(aws-iam-authenticator token -i $CLUSTER_NAME --role arn:aws:iam::322021904188:role/Administrator | jq -r .status.token)
 
 # Output token as JSON
 jq -n --arg token "$TOKEN" '{"token": $token}'

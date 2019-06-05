05eca0b251c380cabf4d94e834495e44636118ee
nullck <nullck@gmail.com>
2019-06-05 16:06:09 -0300

    enabling macos to exec kubectl_apply

--- common/cluster_services/kubectl_apply.sh
+++ common/cluster_services/kubectl_apply.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-echo "${KUBECONFIG_DATA}" | base64 -d > $KUBECONFIG
+if [ "$(uname -s)" == "Darwin" ]; then
+    echo "${KUBECONFIG_DATA}" | base64 -D > $KUBECONFIG
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "${KUBECONFIG_DATA}" | base64 -d > $KUBECONFIG
+fi
 
 if [ -s $1 ]
 then

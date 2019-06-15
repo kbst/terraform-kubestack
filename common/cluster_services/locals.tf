87c43c3dd09843c2a03ea45573623bd09aa20c5f
Philipp Strube <pst@kubestack.com>
2019-06-15 20:25:32 +0200

    Run `terraform 0.12upgrade` for cluster services module

--- common/cluster_services/locals.tf
+++ common/cluster_services/locals.tf
@@ -1,13 +1,14 @@
 locals {
   fqdn_label   = "${var.label_namespace}cluster_fqdn"
-  cluster_fqdn = "${var.metadata_labels[local.fqdn_label]}"
+  cluster_fqdn = var.metadata_labels[local.fqdn_label]
   cluster_dir  = "${path.cwd}/clusters/${local.cluster_fqdn}"
 
   workspace_label = "${var.label_namespace}cluster_workspace"
-  workspace       = "${var.metadata_labels[local.workspace_label]}"
+  workspace       = var.metadata_labels[local.workspace_label]
   build_path      = "manifests/overlays/${var.cluster_type}/${local.workspace}"
 
   output_file = "cluster_services.yaml"
 
   kubeconfig_path = "${local.cluster_dir}/kubeconfig"
 }
+

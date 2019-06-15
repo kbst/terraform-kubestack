36bb4ca747758a248d08502d0d9e6c26943ba3aa
Philipp Strube <pst@kubestack.com>
2019-06-15 19:21:52 +0200

    Update quickstart configurations to use 0.12 syntax

--- quickstart/src/configurations/aks/clusters.tf
+++ quickstart/src/configurations/aks/clusters.tf
@@ -1,5 +1,5 @@
 module "aks_zero" {
   source = "github.com/kbst/terraform-kubestack//azurerm/cluster?ref={{version}}"
 
-  configuration = "${var.clusters["aks_zero"]}"
+  configuration = var.clusters["aks_zero"]
 }
--- quickstart/src/configurations/eks/clusters.tf
+++ quickstart/src/configurations/eks/clusters.tf
@@ -5,5 +5,5 @@ module "eks_zero" {
 
   source = "github.com/kbst/terraform-kubestack//aws/cluster?ref={{version}}"
 
-  configuration = "${var.clusters["eks_zero"]}"
+  configuration = var.clusters["eks_zero"]
 }

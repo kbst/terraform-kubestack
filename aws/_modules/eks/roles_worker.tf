0e4ebef425f32898098f043684552680f866b14d
Philipp Strube <pst@kubestack.com>
2020-10-07 12:49:03 +0200

    Cleanup incorrect Terraform formatting

--- aws/_modules/eks/roles_worker.tf
+++ aws/_modules/eks/roles_worker.tf
@@ -20,17 +20,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_container_registry_ro" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = aws_iam_role.node.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_instance_profile" "nodes" {

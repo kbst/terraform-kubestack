22b0eb8c9439d8f4f2480c04f59acfeba54e6be9
Philipp Strube <pst@kubestack.com>
2021-02-23 14:02:04 +0100

    Support maps of objects in inheritance module
    This change supports both the current `map(map(string)` but
    also `map(map(object({...})))` to unlock the ability to move
    towards a configuration schema that's not limited to maps of
    strings only.
    
    Moving the configuration to ojbects is currently blocked
    by an issue with making object attributes optional.
    
    https://github.com/hashicorp/terraform/issues/27295

--- common/configuration/outputs.tf
+++ common/configuration/outputs.tf
@@ -4,9 +4,16 @@ locals {
   }
 
   overlays = {
-    for name, _ in var.configuration :
-    name => merge(var.configuration[var.base_key], var.configuration[name])
-    if name != var.base_key
+    # include all envs but the base_key in overlays
+    for env_key, env in var.configuration :
+    env_key => {
+      # loop through all config keys in base_key environment
+      # if current env has that key, use the value from current env
+      # if not, use the value from the base_key environment
+      for key, value in var.configuration[var.base_key] :
+      key => lookup(env, key, null) != null ? env[key] : value
+    }
+    if env_key != var.base_key
   }
 }
 

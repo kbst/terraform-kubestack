c6ce5bb62ef6c70836f54fc98db3ffe6ae05779e
Philipp Strube <pst@kubestack.com>
2021-03-13 12:36:49 +0100

    Remove incorrect comment from AKS starter clusters.tf

--- quickstart/src/configurations/aks/clusters.tf
+++ quickstart/src/configurations/aks/clusters.tf
@@ -2,7 +2,4 @@ module "aks_zero" {
   source = "github.com/kbst/terraform-kubestack//azurerm/cluster?ref={{version}}"
 
   configuration = var.clusters["aks_zero"]
-
-  # vnet_subnet_id = azurerm_subnet.external.id  # uncomment and populate with an externally-created
-                                                 # subnet's ID when using CNI/advanced networking
 }

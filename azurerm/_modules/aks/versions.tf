05346965e8724744d8d752f51295ae1240dc3782
Philipp Strube <pst@kubestack.com>
2021-04-16 18:27:24 +0200

    Relax provider version requirements

--- azurerm/_modules/aks/versions.tf
+++ azurerm/_modules/aks/versions.tf
@@ -4,31 +4,31 @@ terraform {
     azurerm = {
       # https://registry.terraform.io/providers/hashicorp/azurerm/latest
       source  = "hashicorp/azurerm"
-      version = "~> 2.45.1"
+      version = ">= 2.45.1"
     }
 
     azuread = {
       # https://registry.terraform.io/providers/hashicorp/azuread/latest
       source  = "hashicorp/azuread"
-      version = "~> 1.3.0"
+      version = ">= 1.3.0"
     }
 
     external = {
       # https://registry.terraform.io/providers/hashicorp/external/latest
       source  = "hashicorp/external"
-      version = "~> 2.0.0"
+      version = ">= 2.0.0"
     }
 
     kubernetes = {
       # https://registry.terraform.io/providers/hashicorp/kubernetes/latest
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0.2"
+      version = ">= 2.0.2"
     }
 
     random = {
       # https://registry.terraform.io/providers/hashicorp/random/latest
       source  = "hashicorp/random"
-      version = "~> 3.0.1"
+      version = ">= 3.0.1"
     }
   }
 

f49c4429163c5425a12697025ed79f6e347f8e32
Philipp Strube <pst@kubestack.com>
2021-06-17 15:16:00 +0200

    Remove cluster services module - deprecated in v0.15.0-beta.0

--- common/cluster_services/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "template_string" {
-  type        = string
-  description = "Kubeconfig template to render."
-}
-
-variable "template_vars" {
-  type        = map(string)
-  description = "Variables the kubeconfig template requires."
-}

302c6dea662c4a74099a68433339420c96481e13
Philipp Strube <pst@kubestack.com>
2021-08-23 17:59:09 +0200

    Fix azuread_service_principal_password, password attribute deprecated

--- azurerm/_modules/aks/service_principal.tf
+++ azurerm/_modules/aks/service_principal.tf
@@ -10,17 +10,9 @@ resource "azuread_service_principal" "current" {
   application_id = azuread_application.current[0].application_id
 }
 
-resource "random_string" "password" {
-  count = var.disable_managed_identities == true ? 1 : 0
-
-  length  = 64
-  special = true
-}
-
 resource "azuread_service_principal_password" "current" {
   count = var.disable_managed_identities == true ? 1 : 0
 
   service_principal_id = azuread_service_principal.current[0].id
-  value                = random_string.password[0].result
   end_date_relative    = var.service_principal_end_date_relative
 }

907c80a7510f8b2b76d147f871fca2fb250706de
Philipp Strube <pst@kubestack.com>
2021-11-08 19:03:18 +0100

    Replace deprecated template_file resource with templatefile function

--- azurerm/_modules/aks/kubeconfig.tf
+++ azurerm/_modules/aks/kubeconfig.tf
@@ -1,7 +1,5 @@
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/templates/kubeconfig.tpl")
-
-  vars = {
+locals {
+  template_vars = {
     cluster_name     = azurerm_kubernetes_cluster.current.name
     cluster_endpoint = azurerm_kubernetes_cluster.current.kube_config[0].host
     cluster_ca       = azurerm_kubernetes_cluster.current.kube_config[0].cluster_ca_certificate
@@ -9,4 +7,6 @@ data "template_file" "kubeconfig" {
     client_key       = azurerm_kubernetes_cluster.current.kube_config[0].client_key
     path_cwd         = path.cwd
   }
+
+  kubeconfig = templatefile("${path.module}/templates/kubeconfig.tpl", local.template_vars)
 }
--- google/_modules/gke/kubeconfig.tf
+++ google/_modules/gke/kubeconfig.tf
@@ -1,14 +1,10 @@
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/templates/kubeconfig.tpl")
-
-  vars = {
+locals {
+  template_vars = {
     cluster_name     = google_container_cluster.current.name
     cluster_endpoint = google_container_cluster.current.endpoint
     cluster_ca       = google_container_cluster.current.master_auth[0].cluster_ca_certificate
     path_cwd         = path.cwd
   }
 
-  # when the node pool is destroyed before the k8s namespaces
-  # the namespaces get stuck in terminating
-  depends_on = [module.node_pool]
+  kubeconfig = templatefile("${path.module}/templates/kubeconfig.tpl", local.template_vars)
 }

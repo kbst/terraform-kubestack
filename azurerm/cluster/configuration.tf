26c19af3528bcc1205fea94c1c3f3f961c64c4df
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2023-07-04 15:04:19 +0200

    Allow passing additional labels/tags to AKS clusters

--- azurerm/cluster/configuration.tf
+++ azurerm/cluster/configuration.tf
@@ -62,4 +62,8 @@ locals {
 
   availability_zones_lookup = lookup(local.cfg, "availability_zones", "")
   availability_zones        = local.availability_zones_lookup != "" ? split(",", local.availability_zones_lookup) : []
+
+  cluster_additional_labels_lookup = lookup(local.cfg, "labels", "")
+  cluster_additional_labels_tuples = [for t in split(",", local.cluster_additional_labels_lookup) : split("=", t)]
+  cluster_additional_labels        = { for t in local.cluster_additional_labels_tuples : t[0] => t[1] if length(t) == 2 }
 }
